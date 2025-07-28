112cf21d37010e8c7e8b4155fced4916ed42b5e3
aalshakarchi <50530363+aalshakarchi@users.noreply.github.com>
2025-07-27 22:54:08 -0500

    Update main.py
    Added listening to $PORT

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from app import app  # noqa: F401
+import os
+from app import app
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    print(f"Listening on {port}", flush=True)
+    app.run(host="0.0.0.0", port=port)
